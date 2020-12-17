f3341e266445be0edc684672fc5a354f1088cf6d
Kors van Loon <k.vanloon@labdigital.nl>
2020-12-17 15:03:51 +0100

    Add suggestion to ListItem as prop.
    closes #3

--- src/components/DefaultListItem.tsx
+++ src/components/DefaultListItem.tsx
@@ -1,6 +1,7 @@
 import { makeStyles } from '@material-ui/core';
 import clsx from 'clsx';
 import React, { LiHTMLAttributes } from 'react';
+import { Item } from '..';
 
 const useStyles = makeStyles({
   listItem: {
@@ -11,12 +12,27 @@ const useStyles = makeStyles({
 });
 
 const DefaultListItem = (
-  props: LiHTMLAttributes<HTMLLIElement>
+  props: LiHTMLAttributes<HTMLLIElement> & { suggestion: Item }
 ): JSX.Element => {
-  const { className, ...rest } = props;
+  const { className, value, children, suggestion, ...rest } = props;
   const classes = useStyles();
 
-  return <li className={clsx(classes.listItem, className)} {...rest} />;
+  const regex = new RegExp(`(.*)(${value as string})(.*)`, 'i');
+
+  const search = `${suggestion.Text} ${suggestion.Description}`;
+
+  const result = search.match(regex);
+  const [before, match, after] = result?.slice(1) ?? ['', '', ''];
+
+  return (
+    <li className={clsx(classes.listItem, className)} {...rest}>
+      <>
+        {before}
+        <strong>{match}</strong>
+        {after}
+      </>
+    </li>
+  );
 };
 
 export default DefaultListItem;
