9592a5ce300553dc2389cd5c91a9d22fa68d2f35
Nic Hines <nic.hines@ttsltd.com>
2024-06-04 13:16:23 +0100

    Update URLs

--- src/constants/loqate.ts
+++ src/constants/loqate.ts
@@ -1,3 +1,5 @@
-export const LOQATE_BASE_URL = 'https://api.addressy.com/Capture/Interactive';
+// export const LOQATE_BASE_URL = 'https://api.addressy.com/Capture/Interactive';
+export const LOQATE_BASE_URL = 'https://services.postcodeanywhere.co.uk/Capture/Interactive';
 export const LOQATE_RETRIEVE_URL = 'Retrieve/v1/json3.ws';
-export const LOQATE_FIND_URL = 'Find/v1.10/json3.ws';
+// export const LOQATE_FIND_URL = 'Find/v1.10/json3.ws';
+export const LOQATE_FIND_URL = 'Find/v1.10/json3ex.ws';
